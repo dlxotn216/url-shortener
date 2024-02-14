edf26c6ec43dbe94639ae3a638a5abb48aa61b6b
taesu <taesu@crscube.co.kr>
2024-02-14 12:00:26 +0900

    Service 구현
    Shorten url 생성 및 저장 서비스 구현/테스트

--- src/main/kotlin/io/taesu/urlshortner/application/ShortenUrlCreateService.kt
+++ src/main/kotlin/io/taesu/urlshortner/application/ShortenUrlCreateService.kt
@@ -1,13 +1,32 @@
 package io.taesu.urlshortner.application
 
+import io.taesu.urlshortner.domain.ShortenUrlEntity
 import io.taesu.urlshortner.domain.ShortenUrlEntityRepository
+import io.taesu.urlshortner.infra.HashGenerateRequest
 import io.taesu.urlshortner.infra.HashGenerator
-import io.taesu.urlshortner.interfaces.ShortenUrlCreateRequest
+import io.taesu.urlshortner.interfaces.dtos.ShortenUrlCreateRequest
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.stereotype.Service
 
 @Service
-class ShortenUrlCreateService {
+class ShortenUrlCreateService(
+    private val hashGenerator: HashGenerator,
+    private val shortenUrlEntityRepository: ShortenUrlEntityRepository,
+) {
     fun create(request: ShortenUrlCreateRequest): String {
-        TODO()
+        return hashGenerator.generate(HashGenerateRequest.empty()).apply {
+            shortenUrlEntityRepository.save(
+                ShortenUrlEntity(
+                    originalUrl = request.originalUrl,
+                    hash = this
+                )
+            )
+        }
     }
 }
+
+@ConfigurationProperties(prefix = "config.app")
+class AppHostConfig @ConstructorBinding constructor(
+    val host: String,
+)
---
+++ src/test/kotlin/io/taesu/urlshortner/application/ShortenUrlCreateServiceTest.kt
@@ -0,0 +1,54 @@
+package io.taesu.urlshortner.application
+
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.SpyK
+import io.mockk.junit5.MockKExtension
+import io.mockk.slot
+import io.taesu.urlshortner.domain.ShortenUrlEntity
+import io.taesu.urlshortner.domain.ShortenUrlEntityRepository
+import io.taesu.urlshortner.infra.HashGenerator
+import io.taesu.urlshortner.interfaces.dtos.ShortenUrlCreateRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+/**
+ * Created by itaesu on 2024/02/14.
+ *
+ * @author Lee Tae Su
+ * @version url-shortner
+ * @since url-shortner
+ */
+@ExtendWith(MockKExtension::class)
+class ShortenUrlCreateServiceTest {
+    @MockK
+    private lateinit var hashGenerator: HashGenerator
+
+    @MockK
+    private lateinit var shortenUrlEntityRepository: ShortenUrlEntityRepository
+
+    @SpyK
+    @InjectMockKs
+    private lateinit var shortenUrlCreateService: ShortenUrlCreateService
+
+    @Test
+    fun `단축 URL 생성 서비스 테스트`() {
+        // given
+        every { hashGenerator.generate(any()) } returns "hash-value"
+        val slot = slot<ShortenUrlEntity>()
+        every { shortenUrlEntityRepository.save(capture(slot)) } answers  { firstArg()}
+
+        // when
+        val result = shortenUrlCreateService.create(ShortenUrlCreateRequest("https://test.com"))
+
+        // then
+        assertThat(result).isEqualTo("hash-value")
+        with(slot.captured) {
+            assertThat(originalUrl).isEqualTo("https://test.com")
+            assertThat(hash).isEqualTo("hash-value")
+        }
+    }
+
+}
