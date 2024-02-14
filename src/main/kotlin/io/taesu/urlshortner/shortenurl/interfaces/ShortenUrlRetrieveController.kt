f60e110e8cb870d58620d767344ae9eb06bcb01d
taesu <taesu@crscube.co.kr>
2024-02-14 15:57:51 +0900

    Redirect controller 추가

--- src/main/kotlin/io/taesu/urlshortner/shortenurl/interfaces/ShortenUrlRetrieveController.kt
+++ src/main/kotlin/io/taesu/urlshortner/shortenurl/interfaces/ShortenUrlRetrieveController.kt
@@ -3,9 +3,11 @@ package io.taesu.urlshortner.shortenurl.interfaces
 import io.taesu.urlshortner.app.dtos.SuccessResponse
 import io.taesu.urlshortner.shortenurl.application.ShortenUrlRetrieveService
 import io.taesu.urlshortner.shortenurl.interfaces.dtos.ShortenUrlRetrieveResponse
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
 
 /**
  * Created by itaesu on 2024/02/14.
@@ -21,5 +23,16 @@ class ShortenUrlRetrieveController(
     @GetMapping("/api/v1/shorten-urls/{hash}")
     fun retrieve(@PathVariable hash: String): SuccessResponse<ShortenUrlRetrieveResponse> {
         return SuccessResponse(ShortenUrlRetrieveResponse(shortenUrlRetrieveService.retrieveOriginalUrl(hash)))
+    }
+}
+
+@Controller
+class ShortenUrlRedirectController(
+    private val shortenUrlRetrieveService: ShortenUrlRetrieveService,
+) {
+    @GetMapping("{hash}")
+    fun redirect(@PathVariable hash: String): RedirectView {
+        val originalUrl = shortenUrlRetrieveService.retrieveOriginalUrl(hash)
+        return RedirectView(originalUrl)
     }
 }
